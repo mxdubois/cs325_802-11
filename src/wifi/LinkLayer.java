0e596e663644698b24c640d11e6b014d9c3c52e4
Michael DuBois <michael@michael-dubois.com>
2013-12-03 23:31:12 -0800

    removed unnecessary null check

--- src/wifi/LinkLayer.java
+++ src/wifi/LinkLayer.java
@@ -178,10 +178,6 @@ public class LinkLayer implements Dot11Interface {
 				return 0;
 			}
 		}
-		
-		// If there are no packets to deliver
-		if(mLastRecvData == null)
-			return 0;
 
 		// Put addresses in Transmission object
 		t.setDestAddr(mLastRecvData.getDestAddr());
